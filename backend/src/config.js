d1424ff2bdafaf4a7ceb09cd482957e7227f80d0
Revant Mohanasundaram <revantmv2008@gmail.com>
2022-04-24 09:29:16 -0400

    19

--- backend/src/config.js
+++ backend/src/config.js
@@ -57,7 +57,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x24CFaC07D0f8C7e5bd6159D65B06979f9F54aa6f';
 const TREASURY_ADDRESS = '0x24CFaC07D0f8C7e5bd6159D65B06979f9F54aa6f';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.08; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 10; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
